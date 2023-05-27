257fd5db5394e9487d6e831bcd30ceab957c817b
Arvindran Kalidas <arvindrankalidas@Arvindrans-MacBook-Pro-2.local>
2023-05-27 17:30:22 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pre-requisites
+- NodeJS
+- Any IDE
+
+# Steps to Execute Script
+- Install all the necessary requirements using the command `npm install`
+- Run the script by using the command `npm run test`
+- The results will be displayed in the console
---
+++ RakutenAssessment.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+const BASE_URL = "https://api.viki.io/v4/videos.json";
+
+async function getVideos() {
+    let hasMore = true;
+    let page = 1
+    let videos = [];
+
+    while (hasMore) {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                app: "100250a",
+                per_page: 10,
+                page: page
+            }
+        })
+
+        videos = [...videos, ...response.data.response];
+        hasMore = response.data.more;
+        page++;
+    }
+
+    return videos;
+}
+
+function getCountOfHd(videos) {
+    let countOfHD = {
+        hd: {
+            true: 0,
+            false: 0
+        }
+    }
+
+    for (let i = 0; i < videos.length; i++) {
+        if (videos[i].flags.hd) {
+            countOfHD.hd.true++;
+        } else {
+            countOfHD.hd.false++;
+        }
+    }
+
+    return countOfHD;
+}
+
+function getCountOfCountries(videos) {
+    let countries = {};
+
+    for (let i = 0; i < videos.length; i++) {
+        if (countries[videos[i].container.origin.country]) {
+            countries[videos[i].container.origin.country]++;
+        } else {
+            countries[videos[i].container.origin.country] = 1;
+        }
+    }
+
+    return countries;
+}
+
+getVideos().then(videos => {
+    console.log(getCountOfHd(videos));
+    console.log(getCountOfCountries(videos));
+});
